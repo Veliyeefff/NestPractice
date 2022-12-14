7c413f55e868b0ea7f25afd6f0381dbc75973e3c
Veliyeefff <6mazgz0@code.edu.az>
2022-12-14 19:10:34 +0400

    Created

---
+++ main.js
@@ -0,0 +1,33 @@
+// Categories Toggle
+let categorie_icon = document.querySelector(".nav-categories")
+let active_menu = document.querySelector(".categories-menu")
+categorie_icon.addEventListener("click",function(e){
+    active_menu.classList.toggle("active")
+})
+
+// Location Toggle
+let location_icon = document.querySelector(".nav-location")
+let active_location_menu = document.querySelector(".location-menu")
+location_icon.addEventListener("click",function(e){
+    active_location_menu.classList.toggle("active")
+})
+
+// BrowseAllCategories
+
+let categories = document.querySelector(".categ_btn")
+let active_categories_menu = document.querySelector(".row")
+categories.addEventListener("click",function(){
+    active_categories_menu.classList.toggle("active-flex")
+})
+
+//mobilemenu
+let menu = document.querySelector(".fa-bars")
+let sidebar = document.querySelector(".sidebar")
+menu.addEventListener("click",function(){
+    sidebar.classList.add("active")
+})
+
+let exit = document.querySelector(".menu-exit")
+exit.addEventListener("click",function(){
+    sidebar.classList.remove("active")
+})
